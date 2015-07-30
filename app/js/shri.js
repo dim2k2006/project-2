7bfa7b725783f42759c7f4fe7072939044e3946a
dim2k2006 <dim2k2006@yandex.ru>
2015-07-31 03:02:33 +0400

    add user dialog

--- app/js/shri.js
+++ app/js/shri.js
@@ -48,52 +48,92 @@ var requests = ['/countries', '/cities', '/populations'];
 var responses = {};
 
 var callback = function (error, result, num) {
-        console.log(result);
+    //console.log(result);
 
-        var name = requests[num];
+    var name = requests[num];
 
-        responses[name] = result;
+    responses[name] = result;
 
-        console.log(responses);
+    //console.log(responses);
 
-        var l = [];
-        for (var K in responses) {
-            l.push(K);
+    var l = [];
+    for (var K in responses) {
+        l.push(K);
+    }
+
+    //console.log(l.length);
+
+    if (l.length == 3) {
+        var c = [], cc = [], p = 0;
+        for (i = 0; i < responses['/countries'].length; i++) {
+            if (responses['/countries'][i].continent === 'Africa') {
+                c.push(responses['/countries'][i].name);
+            }
         }
 
-        //console.log(l);
-
-        if (l.length == 3) {
-            var c = [], cc = [], p = 0;
-            for (i = 0; i < responses['/countries'].length; i++) {
-                if (responses['/countries'][i].continent === 'Africa') {
-                    c.push(responses['/countries'][i].name);
+        for (i = 0; i < responses['/cities'].length; i++) {
+            for (j = 0; j < c.length; j++) {
+                if (responses['/cities'][i].country === c[j]) {
+                    cc.push(responses['/cities'][i].name);
                 }
             }
-
-            for (i = 0; i < responses['/cities'].length; i++) {
-                for (j = 0; j < c.length; j++) {
-                    if (responses['/cities'][i].country === c[j]) {
-                        cc.push(responses['/cities'][i].name);
-                    }
-                }
-            }
-
-            for (i = 0; i < responses['/populations'].length; i++) {
-                for (j = 0; j < cc.length; j++) {
-                    if (responses['/populations'][i].name === cc[j]) {
-                        p += responses['/populations'][i].count;
-                    }
-                }
-            }
-
-            console.log('Total population in African cities: ' + p);
         }
-    };
+
+        for (i = 0; i < responses['/populations'].length; i++) {
+            for (j = 0; j < cc.length; j++) {
+                if (responses['/populations'][i].name === cc[j]) {
+                    p += responses['/populations'][i].count;
+                }
+            }
+        }
+
+        console.log('Total population in African cities: ' + p);
+        console.log('');
+        userDialog(responses);
+    }
+};
 
 for (i = 0; i < 3; i++) {
     var request = requests[i];
-    console.log('request '+i+': '+request);
+    //console.log('request '+i+': '+request);
 
     getData(request, callback, i);
+}
+
+
+
+/**
+ * User dialog
+ */
+function userDialog(responses) {
+    var dialogData = prompt("Введите название страны или города", "Cameroon"),
+        cityObject = '',
+        find = false;
+
+    for (i = 0; i < responses['/populations'].length; i++) {
+        cityObject = responses['/populations'][i];
+        if (cityObject.name === dialogData) {
+            console.log(cityObject.count);
+            find = true;
+        }
+    }
+
+    
+    if (!find) {
+        for (i = 0; i < responses['/cities'].length; i++) {
+            cityObject = responses['/cities'][i];
+            if (cityObject.country === dialogData) {
+                dialogData = cityObject.name;
+            }
+        } 
+
+
+        for (i = 0; i < responses['/populations'].length; i++) {
+            cityObject = responses['/populations'][i];
+            if (cityObject.name === dialogData) {
+                console.log(cityObject.count);
+                find = true;
+            }
+        }
+    }
 }
